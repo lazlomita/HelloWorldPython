77c6eb30fb8a0ca903812d04ab286a00f3c29dfa
lazlo.mita <lazlo.mita@gmail.com>
2018-02-13 16:02:22 -0400

    Add FOR, IF, ELSE, Concatenation.

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,2 +1,16 @@
+#  Compilation:  No copile process needed
+#  Execution:    python HelloWorld.py
+#  Execution:    HelloWorld.py
+#
 # Print Hello world in Python programming language
 print "Hello world PYTHON\n"
+
+# Run a cycle that goes from 1 to 10
+for i in range(1,10):
+	# Decide if the number is even or not
+	if i%2 == 0:
+		# Text concatenation and print
+		print "Count is: " + str(i) + " and it is even."
+	else:
+		# Text concatenation and print
+		print "Count is: " + str(i) + " and it is odd."
